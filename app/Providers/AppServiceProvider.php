d9a5114035a242cc45af5a5157b8e6828d498497
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-09-17 21:54:40 +0300

    `Refactor CustomResetPassword notification to use createUrlUsing method`

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url') . "/auth/reset-password?token={$token}&email=" . urlencode($notifiable->getEmailForPasswordReset());
-        });
-
         Schema::defaultStringLength(191);
     }
 }
