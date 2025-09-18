c24f116fab009ea8b340f4856b9ea5631f515c97
zeyad.h.abaza <zeyad.h.abaza@gmail.com>
2025-09-18 21:41:26 +0300

    `Added authentication middleware to push subscription routes`

--- routes/notification.php
+++ routes/notification.php
@@ -2,6 +2,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebPushController;
 
-// Add these routes to handle push subscriptions
-Route::post('/push/subscribe', [WebPushController::class, 'subscribe'])->name('push.subscribe');
-Route::post('/push/unsubscribe', [WebPushController::class, 'unsubscribe'])->name('push.unsubscribe');
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Add these routes to handle push subscriptions
+    Route::post('/push/subscribe', [WebPushController::class, 'subscribe'])->name('push.subscribe');
+    Route::post('/push/unsubscribe', [WebPushController::class, 'unsubscribe'])->name('push.unsubscribe');
+});
